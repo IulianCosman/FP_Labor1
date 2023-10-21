eb46817a734b8336dd446fac8c00ca1981482f0f
home <iuliancosman709@gmail.com>
2023-10-20 17:43:25 -0700

    Final version

--- src/Problem3.java
+++ src/Problem3.java
@@ -21,7 +21,8 @@ public class Problem3 {
 
     public static int[] differenz(int[] number1, int[] number2){
         int[] subtraction = new int[number1.length];
-        boolean firstBigger = false, carriedOver = false;
+        int carriedOver = 0;
+        boolean firstBigger = false;
         int cnt = 0;
         while(number1[cnt] == number2[cnt] && cnt < number1.length-1){
             cnt++;
@@ -31,20 +32,17 @@ public class Problem3 {
         }
         for(int i = number1.length - 1; i >= 0; i--){
             if(firstBigger) {
-                subtraction[i] = number1[i] - number2[i];
+                subtraction[i] = number1[i] - number2[i] - carriedOver;
             }
             else{
-                subtraction[i] = number2[i] - number1[i];
-            }
-            if(carriedOver){
-                subtraction[i]--;
+                subtraction[i] = number2[i] - number1[i] - carriedOver;
             }
             if(subtraction[i] < 0){
                 subtraction[i] += 10;
-                carriedOver = true;
+                carriedOver = 1;
             }
             else{
-                carriedOver = false;
+                carriedOver = 0;
             }
         }
         if(subtraction[0] == 0){
@@ -71,13 +69,8 @@ public class Problem3 {
         for(int i = number1.length - 1; i >= 0; i--){
             product[i] = number1[i] * number2;
             product[i] += carriedOver;
-            if(product[i] > 9){
-                product[i] = product[i] % 10;
-                carriedOver = product[i] / 10;
-            }
-            else{
-                carriedOver = 0;
-            }
+            carriedOver = product[i] / 10;
+            product[i] = product[i] % 10;
         }
         if(carriedOver != 0){
             int[] newProduct = new int[number1.length + 1];
@@ -108,7 +101,7 @@ public class Problem3 {
         }
         if(division[0] == 0){
             int[] newDivision = new int[number1.length - 1];
-            for(int i = 0; i < number1.length; i++){
+            for(int i = 0; i < number1.length - 1; i++){
                 newDivision[i] = division[i+1];
             }
             return newDivision;
